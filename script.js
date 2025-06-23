7d0fcd228ae5ddfee78a034de3ab96655f70cd16
Nina <134860155+cc-nina@users.noreply.github.com>
2025-06-22 21:33:59 -0400

    error is being returned

---
+++ script.js
@@ -0,0 +1,18 @@
+/*import { createClient } from '/node_modules/@supabase/supabase-js/dist/module/SupabaseClient.js'
+console.log("horse")
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://xhtkarqvsqbdzsbpjaox.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhodGthcnF2c3FiZHpzYnBqYW94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2MjMwNjcsImV4cCI6MjA2NjE5OTA2N30.TVXWNlDRTcV_AAlL1QweZtnsOQtoHpZ62eaxbEG508U')
+
+const { data, error } = await supabase  
+    .from('gostations')  
+    .select('id', 'stat_name', 'colour', 'line_abbr')
+
+console.log(data.data.id[0])
+console.log("horse")*/
+
+
+
+
+
+    
