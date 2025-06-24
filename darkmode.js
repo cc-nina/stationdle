e045f8b589e3529bf8d5026e8e6a5752dff847cf
Nina <134860155+cc-nina@users.noreply.github.com>
2025-06-24 16:36:51 -0400

    dark mode for map

--- darkmode.js
+++ darkmode.js
@@ -4,12 +4,20 @@ const themeSwitch = document.getElementById('themeswitch')
 const enableDarkMode = () => {
     document.body.classList.add('darkmode')
     localStorage.setItem('darkmode', 'active')
-    
+    map.addLayer(dark_tile)
+    if (map.hasLayer(light_tile)) {
+        
+        map.removeLayer(light_tile)
+    }
 }
 
 const disableDarkMode = () => {
     document.body.classList.remove('darkmode')
     localStorage.setItem('darkmode', null)
+    if (map.hasLayer(dark_tile)) {
+        map.addLayer(light_tile)
+        map.removeLayer(dark_tile)
+    }
 }
 
 if (darkmode === 'active') enableDarkMode()
