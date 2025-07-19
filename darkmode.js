33e69b364b67cb2e3b7ac10462240addb442874f
Nina <134860155+cc-nina@users.noreply.github.com>
2025-07-19 11:03:53 -0400

    change results map to correct colour

--- darkmode.js
+++ darkmode.js
@@ -4,19 +4,31 @@ const themeSwitch = document.getElementById('themeswitch')
 const enableDarkMode = () => {
     document.body.classList.add('darkmode')
     localStorage.setItem('darkmode', 'active')
+
     map.addLayer(dark_tile)
     if (map.hasLayer(light_tile)) {
         
         map.removeLayer(light_tile)
+    }
+
+    resultsMap.addLayer(dark_tile_res)
+    if (resultsMap.hasLayer(light_tile_res)) {
+        
+        resultsMap.removeLayer(light_tile_res)
     }
 }
 
 const disableDarkMode = () => {
     document.body.classList.remove('darkmode')
     localStorage.setItem('darkmode', null)
+
     if (map.hasLayer(dark_tile)) {
         map.addLayer(light_tile)
         map.removeLayer(dark_tile)
+    }
+    if (resultsMap.hasLayer(dark_tile_res)) {
+        resultsMap.addLayer(light_tile_res)
+        resultsMap.removeLayer(dark_tile_res)
     }
 }
 
