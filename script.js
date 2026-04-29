fa214f9f6fa73c39a61d1c8378b209bcd2ddc645
Nina <nina067312@gmail.com>
2026-04-29 19:55:25 -0400

    edit key name

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 const { createClient } = supabase
 const _supabase = createClient(supabaseUrl, supabaseKey);
